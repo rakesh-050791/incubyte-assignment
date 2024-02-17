f379143222b3b01f80c8024835b105da2b85225c
rakesh050791 <rakesh050791@gmail.com>
2024-02-17 07:03:10 +0100

    Added implementation for ignoring negarting and numbers greater then 10000, and added respective tests

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,5 +1,12 @@
 class StringCalculator < ApplicationRecord
     def self.add(numbers)
-        numbers.split(',').map(&:to_i).sum
+        delimiters = [',', '\n']
+        numbers = numbers.gsub(/\/\/(.*)\n/, '')
+        delimiters << $1 unless $1.nil?
+        numbers = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+        negatives = numbers.select { |number| number < 0 }
+        raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+      
+        numbers.reject { |number| number > 1000 }.sum
     end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -17,5 +17,25 @@ RSpec.describe StringCalculator, type: :model do
         expect(StringCalculator.add("1,2")).to eq(3)
       end
     end
+
+    context "with negative numbers" do
+      it "throws an exception for a single negative number" do
+        expect { StringCalculator.add("-1") }.to raise_error(RuntimeError, "negatives not allowed: -1")
+      end
+    
+      it "throws an exception listing all negative numbers" do
+        expect { StringCalculator.add("2,-4,3,-5") }.to raise_error(RuntimeError, "negatives not allowed: -4, -5")
+      end
+    end
+
+    context "with numbers greater than 1000" do
+      it "ignores numbers greater than 1000 in the sum" do
+        expect(StringCalculator.add("2,1001")).to eq(2)
+      end
+    
+      it "considers numbers exactly 1000" do
+        expect(StringCalculator.add("1000,2")).to eq(1002)
+      end
+    end
   end
 end
